74e205ef6851ecbfd4a9cb4b681de11597fe7cf8
maxime lu <merdeth69@gmail.com>
2023-12-22 14:47:19 +0100

    :wrench: lel

--- api.py
+++ api.py
@@ -8,6 +8,7 @@ import re
 import mlflow
 from fastapi import FastAPI
 import uvicorn
+from sklearn import metrics
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -15,10 +16,12 @@ app = FastAPI()
 url = "https://bosbinvsnempbohyiwjy.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvc2JpbnZzbmVtcGJvaHlpd2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwNjY2OTQsImV4cCI6MjAxODY0MjY5NH0.1_2dP0dRw2tSiEjervRFTXWftM77DbTk3IHyT6C1Rcc"
 supabase: Client = create_client(url, key)
+data = pd.read_csv('data.csv')
 
 def get_data():
     response = supabase.from_('data').select('*').execute()
     df = pd.DataFrame(response.data)
+    return df
 
 def post_data(title, body, pred):
     data, count = supabase.table('data').insert({"Title": title, "Body": body, "Tags": str(pred)}).execute()
@@ -73,6 +76,23 @@ def predict(All: str):
     convert = ["imag, button, text, view, color, page, use, click, css, element", "string, convert, charact, format, use, return, valu, like, split, way", "tabl, column, sql, select, datafram, date, databas, queri, row, data", "android, gradl, com, studio, build, app, project, googl, apk, use", "instal, packag, npm, python, usr, pip, version, gem, run, command", "file, line, directori, use, project, path, folder, command, get, tri", "array, numpi, object, element, function, valu, int, use, list, loop", "class, public, int, return, method, object, static, void, type, privat", "request, server, http, web, api, net, use, json, post, respons", "use, differ, code, function, like, get, run, way, would, test"]
     pred = convert[pred[0]]
 
+    df = data
+
+    true_labels = df['Tfidf Clus Label']  # Replace this with your actual true labels
+
+    # Assuming 'pickled_model' is your trained model
+    pred = pickled_model.predict(stem(All))  # Replace 'stem(All)' with your actual data preprocessing
+
+    # Calculate metrics
+    accuracy = metrics.accuracy_score(true_labels, pred)
+    f1_score = metrics.f1_score(true_labels, pred)
+    recall = metrics.recall_score(true_labels, pred)
+
+    # Print the metrics
+    print("Accuracy:", accuracy)
+    print("F1 Score:", f1_score)
+    print("Recall:", recall)
+
     with mlflow.start_run():
         mlflow.sklearn.log_model(pickled_model, "model")
 
@@ -81,8 +101,9 @@ def predict(All: str):
 
         # Log additional information
         mlflow.log_param("prediction", pred)
-
-
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("f1_score", f1_score)
+        mlflow.log_metric("recall", recall)
     return pred
 
 if __name__ == "__main__":

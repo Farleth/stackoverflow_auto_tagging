0c42ff9ac6b60c64089cea43b28990c829a379ec
maxime lu <merdeth69@gmail.com>
2023-12-22 13:24:59 +0100

    putin

--- api.py
+++ api.py
@@ -1,22 +1,16 @@
-from stackapi import StackAPI
 import pandas as pd
-import os
+import pickle
 from supabase import create_client, Client
-from sklearn.metrics import silhouette_samples, silhouette_score
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.cluster import KMeans
 from nltk.corpus import stopwords
-import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
-from sklearn import metrics
-import matplotlib.cm as cm
 import pandas as pd
-import numpy as np
-import random
 import nltk
 import re
 import mlflow
+from fastapi import FastAPI
+import uvicorn
+
+# Initialize FastAPI app
+app = FastAPI()
 
 url = "https://bosbinvsnempbohyiwjy.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvc2JpbnZzbmVtcGJvaHlpd2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwNjY2OTQsImV4cCI6MjAxODY0MjY5NH0.1_2dP0dRw2tSiEjervRFTXWftM77DbTk3IHyT6C1Rcc"
@@ -27,8 +21,8 @@ def get_data():
     df = pd.DataFrame(response.data)
 
 def post_data(title, body, pred):
-    data, count, a = supabase.table('data').insert({"Title": title, "Body": body, "Tags": str(pred)}).execute()
-    print("Posting data response ", data, count, a)
+    data, count = supabase.table('data').insert({"Title": title, "Body": body, "Tags": str(pred)}).execute()
+    print("Posting data response ", data, count)
 
 def cleanhtml(sentence): #function to clean the word of any html-tags
     cleanr = re.compile('<.*?>')
@@ -68,3 +62,28 @@ def stem(text: str):
     final_string.append(str1)
 
     return pd.Series(final_string[0].decode('utf8'))
+
+pickled_model = pickle.load(open('notebooks/supervised.pkl', 'rb'))
+
+
+@app.get("/predict_tag")
+def predict(All: str):
+
+    pred = pickled_model.predict(stem(All))
+    convert = ["imag, button, text, view, color, page, use, click, css, element", "string, convert, charact, format, use, return, valu, like, split, way", "tabl, column, sql, select, datafram, date, databas, queri, row, data", "android, gradl, com, studio, build, app, project, googl, apk, use", "instal, packag, npm, python, usr, pip, version, gem, run, command", "file, line, directori, use, project, path, folder, command, get, tri", "array, numpi, object, element, function, valu, int, use, list, loop", "class, public, int, return, method, object, static, void, type, privat", "request, server, http, web, api, net, use, json, post, respons", "use, differ, code, function, like, get, run, way, would, test"]
+    pred = convert[pred[0]]
+
+    with mlflow.start_run():
+        mlflow.sklearn.log_model(pickled_model, "model")
+
+        # Save the MLflow run ID for reference
+        run_id = mlflow.active_run().info.run_id
+
+        # Log additional information
+        mlflow.log_param("prediction", pred)
+
+
+    return pred
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
